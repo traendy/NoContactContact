34bcd2d85524074de06a57b481b0b5919b19c558
Sönke Peters <speters@eos-uptrade.de>
2020-11-20 19:53:12 +0100

    removed permission Rule

--- app/src/androidTest/java/de/traendy/nocontact/TestUtils.kt
+++ app/src/androidTest/java/de/traendy/nocontact/TestUtils.kt
@@ -1,15 +1,12 @@
 package de.traendy.nocontact
 
-import android.os.Build
 import android.os.Environment.DIRECTORY_PICTURES
 import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.runner.screenshot.BasicScreenCaptureProcessor
 import androidx.test.runner.screenshot.Screenshot
-import org.junit.rules.TestRule
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
-import org.junit.runners.model.Statement
 import java.io.File
 import java.io.IOException
 
@@ -57,45 +54,5 @@ class ScreenshotTestWatcher : TestWatcher() {
         val parentFolderPath = "success/${description.className}"
         takeScreenshot(parentFolderPath = parentFolderPath, screenShotName = description.methodName)
         super.finished(description)
-    }
-}
-
-/**
- * This rule adds selected permissions to test app
- */
-class PermissionsRule(private val permissions: Array<String>) : TestRule {
-
-    override fun apply(base: Statement, description: Description?): Statement {
-        return object : Statement() {
-            @Throws(Throwable::class)
-            override fun evaluate() {
-                allowPermissions()
-                base.evaluate()
-                revokePermissions()
-            }
-        }
-    }
-
-    private fun allowPermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            for (permission: String in permissions) {
-                getInstrumentation().uiAutomation.executeShellCommand(
-                        "pm grant " + getInstrumentation().targetContext.packageName
-                                .toString() + " " + permission
-                ).use {}
-
-            }
-        }
-    }
-
-    private fun revokePermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            for (permission: String in permissions) {
-                getInstrumentation().uiAutomation.executeShellCommand(
-                        ("pm revoke " + getInstrumentation().targetContext.packageName
-                                .toString() + " " + permission)
-                ).use {}
-            }
-        }
     }
 }
