53be8c55813b40fe9a8546fde8eaf70e90a802dc
Sönke Peters <speters@eos-uptrade.de>
2020-11-21 19:03:32 +0100

    removed permission custom screencapture processor

--- app/src/androidTest/java/de/traendy/nocontact/TestUtils.kt
+++ app/src/androidTest/java/de/traendy/nocontact/TestUtils.kt
@@ -1,33 +1,15 @@
 package de.traendy.nocontact
 
-import android.os.Environment.DIRECTORY_PICTURES
 import android.util.Log
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.runner.screenshot.BasicScreenCaptureProcessor
 import androidx.test.runner.screenshot.Screenshot
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
-import java.io.File
 import java.io.IOException
 
 const val TAG = "TestUtils"
 
-class IDTScreenCaptureProcessor(parentFolderPath: String) : BasicScreenCaptureProcessor() {
-
-    init {
-        this.mDefaultScreenshotPath = File(
-                File(
-                        getInstrumentation().targetContext.getExternalFilesDir(DIRECTORY_PICTURES),
-                        "noCoNoCoTests"
-                ).absolutePath,
-                "screenshots/$parentFolderPath"
-        )
-    }
-
-    override fun getFilename(prefix: String): String = prefix
-}
-
-fun takeScreenshot(parentFolderPath: String = "", screenShotName: String) {
+fun takeScreenshot(screenShotName: String) {
     Log.d(TAG, "Taking screenshot of '$screenShotName'")
     val screenCapture = Screenshot.capture()
     val processors = setOf(BasicScreenCaptureProcessor())
@@ -44,15 +26,19 @@ fun takeScreenshot(parentFolderPath: String = "", screenShotName: String) {
 
 class ScreenshotTestWatcher : TestWatcher() {
 
+    private var failed = false
+
     override fun failed(e: Throwable?, description: Description) {
-        val parentFolderPath = "failures2/${description.className}"
-        takeScreenshot(parentFolderPath = parentFolderPath, screenShotName = description.methodName)
+        takeScreenshot(screenShotName = description.methodName)
+        failed = true
         super.failed(e, description)
     }
 
     override fun finished(description: Description) {
-        val parentFolderPath = "success/${description.className}"
-        takeScreenshot(parentFolderPath = parentFolderPath, screenShotName = description.methodName)
+        if (!failed) {
+            takeScreenshot(screenShotName = description.methodName)
+        }
+        failed = false
         super.finished(description)
     }
 }
